354682f6627c6c3dda695f027ff44a86bdd5f4cf
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-10-29 09:35:35 +1030

    completed prompts for user input

--- app.js
+++ app.js
@@ -9,9 +9,140 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
 
+// declare global variables
+let teamName = "";
+let internsArr = [];
+let engineersArr = [];
+// for setting a default id
+let currentID = 0;
 
-// Write code to use inquirer to gather information about the development team members,
+// prompt for user to select employee type or end selection
+function employeeTypeQuestion(){
+    console.log("\nAdd team member:");
+    inquirer.prompt({
+        name: "employee",
+        message: "Employee type:",
+        type: "list",
+        choices: ["intern", "engineer",new inquirer.Separator(), "done"]})
+.then(answers => {
+    // if intern selected run intern questions
+    if (answers.employee === "intern"){
+        internQuestion();
+    // if engineer selected run engineer questions
+    } else if (answers.employee === "engineer"){
+        engineerQuestion();
+    }
+    // if done selected program will stop
+}).catch(error => {
+    console.error(error);
+})
+};
+
+// manager prompt that will run first before any other prompts
+function ManagerQuestion(){
+    // increase default id by 1
+    currentID++;
+    inquirer.prompt([{
+        name: "team",
+        message: "Team name:",
+        type: "input",
+        default: "My Team"},
+        {name: "name",
+        message: "Managers name:",
+        type: "input"},
+        {name: "id",
+        message: "ID:",
+        type: "Number",
+        default: currentID},
+        {name: "email",
+        message: "Email:",
+        type: "input"},
+        {name: "officeNumber",
+        message: "Office number:",
+        type: "number"}])
+.then( answers => {
+    // create new manager object
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    // save team name
+    teamName = answers.team;
+    // set the current id (if the user enters a custom id)
+    currentID = answers.id;
+    // run prompt to generate another profile 
+    employeeTypeQuestion();
+}).catch(error => {
+    console.error(error);
+})
+};
+
+function internQuestion(){
+    console.log("\nIntern details:");
+    currentID++;
+    inquirer.prompt([{
+        name: "name",
+        message: "Intern name:",
+        type: "input"},
+        {name: "id",
+        message: "ID:",
+        type: "number",
+        default: currentID},
+        {name: "email",
+        message: "Email:",
+        type: "input"},
+        {name: "school",
+        message: "School:",
+        type: "input"}])
+.then( answers => {
+    // create new intern object
+    const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+    // add intern to array
+    internsArr.push(intern);
+    // set the current id (if the user enters a custom id)
+    currentID = answers.id;
+    // run prompt to generate another profile 
+    employeeTypeQuestion();
+}).catch(error => {
+    console.error(error);
+});
+
+};
+
+// questions for creating an engineer profile
+function engineerQuestion(){
+    console.log("\nEngineer details:");
+    // set the new default id
+    currentID++;
+    inquirer.prompt([{
+        name: "name",
+        message: "Engineer name:",
+        type: "input"},
+        {name: "id",
+        message: "ID:",
+        type: "number",
+        default: currentID},
+        {name: "email",
+        message: "Email:",
+        type: "input"},
+        {name: "github",
+        message: "GitHub username:",
+        type: "input"}])
+.then( answers => {
+    // create new engineer object
+    const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+    // add engineer to an array
+    engineersArr.push(engineer);
+    // set the current id (if the user enters a custom id)
+    currentID = answers.id;
+    // run prompt to generate another profile 
+    employeeTypeQuestion();
+}).catch(error => {
+    console.error(error);
+})
+};
+ 
+// call the first lot of questions
+ManagerQuestion();
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
